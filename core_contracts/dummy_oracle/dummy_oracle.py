0f10a7df3b2b103e877b90821adcc29adb5b7db4
sudeep simkhada <simkhadasudeep9@gmail.com>
2021-03-12 20:42:10 +0545

    add collateral and repay loans tests update

--- core_contracts/dummy_oracle/dummy_oracle.py
+++ core_contracts/dummy_oracle/dummy_oracle.py
@@ -19,5 +19,6 @@ class DummyOracle(IconScoreBase):
     def get_reference_data(self, _base: str, _quote: str) -> dict:
         return {"rate": 597955725813433531, "last_update_base": 1602202275702605, "last_update_quote": 1602202190000000}
 
+    # todo add a setter method to update exchange  rate if sicx
     def fallback(self):
         pass
