abefe17666c50151e6315fb730a5a04deb614852
sudeep simkhada <simkhadasudeep9@gmail.com>
2021-04-08 15:39:58 +0545

    adding methods in governance

--- core_contracts/governance/governance.py
+++ core_contracts/governance/governance.py
@@ -160,6 +160,54 @@ class Governance(IconScoreBase):
         rewards.addNewDataSource(_name, dex_address)
 
     @external
+    @only_owner
+    def setbnUSD(self, _address: Address) -> None:
+        baln = self.create_interface_score(self.addresses['baln'], BalancedInterface)
+        baln.setbnUSD(_address)
+
+    @external
+    @only_owner
+    def setDividends(self, _score: Address) -> None:
+        baln = self.create_interface_score(self.addresses['baln'], BalancedInterface)
+        baln.setDividends(_score)
+
+    @external
+    @only_owner
+    def balancesetDex(self, _address: Address) -> None:
+        baln = self.create_interface_score(self.addresses['baln'], BalancedInterface)
+        baln.setDex(_address)
+
+    @external
+    @only_owner
+    def balancesetOracleName(self, _name: str) -> None:
+        baln = self.create_interface_score(self.addresses['baln'], BalancedInterface)
+        baln.setOracleName(_name)
+
+    @external
+    @only_owner
+    def balancesetMinInterval(self,  _interval: int) -> None:
+        baln = self.create_interface_score(self.addresses['baln'], BalancedInterface)
+        baln.setOracleName(_interval)
+
+    @external
+    @only_owner
+    def balancetoggleStakingEnabled(self) -> None:
+        baln = self.create_interface_score(self.addresses['baln'], BalancedInterface)
+        baln.toggleStakingEnabled()
+
+    @external
+    @only_owner
+    def balancesetMinimumStake(self, _amount: int) -> None:
+        baln = self.create_interface_score(self.addresses['baln'], BalancedInterface)
+        baln.setMinimumStake(_amount)
+
+    @external
+    @only_owner
+    def balancesetUnstakingPeriod(self, _time: int) -> None:
+        baln = self.create_interface_score(self.addresses['baln'], BalancedInterface)
+        baln.setUnstakingPeriod(_time)
+
+    @external
     def tokenFallback(self, _from: Address, _value: int, _data: bytes) -> None:
         """
         Used only to receive sICX for unstaking.
