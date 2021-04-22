93631793a8d5b6019db97764179d2456d42901a6
Sudeep Bhandari <bhandari.sudeep68@gmail.com>
2021-04-22 18:51:25 +0545

    update typehint from str to int on lpmetadatadb

--- core_contracts/dex/lp_metadata.py
+++ core_contracts/dex/lp_metadata.py
@@ -8,7 +8,7 @@ class LPMetadataDB:
         self._metadata = {}
         self._db = db
 
-    def __getitem__(self, _id: str) -> SetDB:
+    def __getitem__(self, _id: int) -> SetDB:
         if _id not in self._metadata:
             self._metadata[_id] = SetDB(f"lp{_id}", self._db, value_type=Address, order=True)
 
