61cb86d55d984a736d8c9c835813e260a749d514
Suyog <adhikarisuyog49@gmail.com>
2021-04-22 17:33:31 +0545

    consts file formatted

--- core_contracts/staking/utils/checks.py
+++ core_contracts/staking/utils/checks.py
@@ -1,56 +1,59 @@
-
 from .consts import *
+
 
 # ================================================
 #  Exceptions
 # ================================================
 
 class SenderNotScoreOwnerError(Exception):
-	pass
+    pass
 
 
 class SenderNotAuthorized(Exception):
-	pass
+    pass
 
 
 class NotAFunctionError(Exception):
-	pass
+    pass
 
 
 def only_admin(func):
-	if not isfunction(func):
-		raise NotAFunctionError
+    if not isfunction(func):
+        raise NotAFunctionError
 
-	@wraps(func)
-	def __wrapper(self: object, *args, **kwargs):
-		if self.msg.sender != self._admin.get():
-			raise SenderNotAuthorized(self.msg.sender)
+    @wraps(func)
+    def __wrapper(self: object, *args, **kwargs):
+        if self.msg.sender != self._admin.get():
+            raise SenderNotAuthorized(self.msg.sender)
 
-		return func(self, *args, **kwargs)
-	return __wrapper
+        return func(self, *args, **kwargs)
+
+    return __wrapper
 
 
 def only_owner(func):
-	if not isfunction(func):
-		raise NotAFunctionError
+    if not isfunction(func):
+        raise NotAFunctionError
 
-	@wraps(func)
-	def __wrapper(self: object, *args, **kwargs):
-		if self.msg.sender != self.owner:
-			raise SenderNotScoreOwnerError(self.owner)
+    @wraps(func)
+    def __wrapper(self: object, *args, **kwargs):
+        if self.msg.sender != self.owner:
+            raise SenderNotScoreOwnerError(self.owner)
 
-		return func(self, *args, **kwargs)
-	return __wrapper
+        return func(self, *args, **kwargs)
+
+    return __wrapper
 
 
 def staking_on(func):
-	if not isfunction(func):
-		revert(f"NotAFunctionError")
+    if not isfunction(func):
+        revert(f"NotAFunctionError")
 
-	@wraps(func)
-	def __wrapper(self: object, *args, **kwargs):
-		if not self._staking_on.get():
-			revert(f'{TAG}: ICX Staking SCORE is not active.')
+    @wraps(func)
+    def __wrapper(self: object, *args, **kwargs):
+        if not self._staking_on.get():
+            revert(f'{TAG}: ICX Staking SCORE is not active.')
 
-		return func(self, *args, **kwargs)
-	return __wrapper
+        return func(self, *args, **kwargs)
+
+    return __wrapper
