2d4410eacfc8545c2abf5443221e56de3257175a
Dan Brehmer <dan@brehmer.org>
2021-04-25 17:04:12 -0700

    fixed pool creation.

--- core_contracts/governance/governance.py
+++ core_contracts/governance/governance.py
@@ -38,13 +38,6 @@ class Governance(IconScoreBase):
 
     def on_update(self) -> None:
         super().on_update()
-        # Resetting first day rewards recipients split
-        RECIPIENTS.clear()
-        RECIPIENTS.extend([{'recipient_name': 'Loans', 'dist_percent': 25 * 10**16},
-                           {'recipient_name': 'sICX/ICX', 'dist_percent': 10 * 10**16},
-                           {'recipient_name': 'Worker Tokens', 'dist_percent': 20 * 10**16},
-                           {'recipient_name': 'Reserve Fund', 'dist_percent': 5 * 10**16},
-                           {'recipient_name': 'DAOfund', 'dist_percent': 40 * 10**16}])
 
     @external(readonly=True)
     def name(self) -> str:
@@ -122,9 +115,12 @@ class Governance(IconScoreBase):
         pid = dex.getPoolId(sICX_address, bnUSD_address)
         dex.setMarketName(pid, name)
         rewards.addNewDataSource(name, dex_address)
-        recipients = RECIPIENTS[:]
-        recipients.append({'recipient_name': 'sICX/bnUSD', 'dist_percent': 175 * 10**15})
-        recipients[4]['dist_percent'] = 225 * 10**15
+        recipients = [{'recipient_name': 'Loans', 'dist_percent': 25 * 10**16},
+                      {'recipient_name': 'sICX/ICX', 'dist_percent': 10 * 10**16},
+                      {'recipient_name': 'Worker Tokens', 'dist_percent': 20 * 10**16},
+                      {'recipient_name': 'Reserve Fund', 'dist_percent': 5 * 10**16},
+                      {'recipient_name': 'DAOfund', 'dist_percent': 225 * 10**15},
+                      {'recipient_name': 'sICX/bnUSD', 'dist_percent': 175 * 10**15}]
         rewards.updateBalTokenDistPercentage(recipients)
 
     @external
@@ -147,10 +143,13 @@ class Governance(IconScoreBase):
         pid = dex.getPoolId(baln_address, bnUSD_address)
         dex.setMarketName(pid, name)
         rewards.addNewDataSource(name, dex_address)
-        recipients = RECIPIENTS[:]
-        recipients.append({'recipient_name': 'sICX/bnUSD', 'dist_percent': 175 * 10**15})
-        recipients.append({'recipient_name': 'BALN/bnUSD', 'dist_percent': 175 * 10**15})
-        recipients[4]['dist_percent'] = 5 * 10**16
+        recipients = [{'recipient_name': 'Loans', 'dist_percent': 25 * 10**16},
+                      {'recipient_name': 'sICX/ICX', 'dist_percent': 10 * 10**16},
+                      {'recipient_name': 'Worker Tokens', 'dist_percent': 20 * 10**16},
+                      {'recipient_name': 'Reserve Fund', 'dist_percent': 5 * 10**16},
+                      {'recipient_name': 'DAOfund', 'dist_percent': 5 * 10**16},
+                      {'recipient_name': 'sICX/bnUSD', 'dist_percent': 175 * 10**15},
+                      {'recipient_name': 'BALN/bnUSD', 'dist_percent': 175 * 10**15}]
         rewards.updateBalTokenDistPercentage(recipients)
 
     @external
