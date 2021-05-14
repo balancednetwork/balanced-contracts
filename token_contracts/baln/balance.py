e4a4f3a267b628f5507de719b1b99158a5a50d01
Dan Brehmer <dan@brehmer.org>
2021-05-14 04:31:09 -0700

    add on_update method to move tokens to new div SCORE.

--- token_contracts/baln/balance.py
+++ token_contracts/baln/balance.py
@@ -121,6 +121,10 @@ class BalancedToken(IRC2):
 
     def on_update(self) -> None:
         super().on_update()
+        old_div_address = Address.from_string('cx13f08df7106ae462c8358066e6d47bb68d995b6d')
+        new_div_address = Address.from_string('cx203d9cd2a669be67177e997b8948ce2c35caffae')
+        old_div_balance = self._balances[old_div_address]
+        self._transfer(old_div_address, new_div_address, old_div_balance)
 
     @external(readonly=True)
     def getPeg(self) -> str:
