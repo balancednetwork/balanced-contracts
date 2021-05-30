1b4ad3c411354987ddf100333dad9ef1326070b0
sudeep simkhada <simkhadasudeep9@gmail.com>
2021-05-30 17:00:30 +0545

    liquidation test update

--- tests/stories/loans/liquidation_stories.py
+++ tests/stories/loans/liquidation_stories.py
@@ -5,24 +5,20 @@ LIQUIDATION_STORIES = {
                                "bnusd loan",
                 "actions": {
                     "deposited_icx": 782769 * ICX // 1000,
-                    "test_icx": 600,
-                    "test_bnUSD": 2000 * ICX,
-                    "expected_initial_position": "No Debt",
+                    "expected_initial_position": "Not Mining",
                     "expected_position": "Liquidate",
                     "expected_result": "Zero"
                 }
-            },
-                {
-                    "description": "liquidating btest_wallet account by depositing 782 icx collateral and minting "
-                                   "1500 bnusd loan",
-                    "actions": {
-                        "deposited_icx": 782769 * ICX // 1000,
-                        "test_icx": 600,
-                        "test_bnUSD": 1500 * ICX,
-                        "expected_initial_position": "No Debt",
-                        "expected_position": "Liquidate",
-                        "expected_result": "Zero"
-                    }
-                }
+            }
+                # {
+                #     "description": "liquidating btest_wallet account by depositing 782 icx collateral and minting "
+                #                    "1500 bnusd loan",
+                #     "actions": {
+                #         "deposited_icx": 782769 * ICX // 1000,
+                #         "expected_initial_position": "No Debt",
+                #         "expected_position": "Liquidate",
+                #         "expected_result": "Zero"
+                #     }
+                # }
             ]
         }
--- tests/test_integrate_loans_liquidation.py
+++ tests/test_integrate_loans_liquidation.py
@@ -1,8 +1,39 @@
-from .test_integrate_base import BalancedTestBase
+import subprocess
+import time
+
+from core_contracts.loans.loans.loans import Loans
+from .test_integrate_base_liquidation import BalancedTestBaseLiquidation
 from tests.stories.loans.liquidation_stories import LIQUIDATION_STORIES
 
 
-class BalancedTestLiquidation(BalancedTestBase):
+class BalancedTestLiquidation(BalancedTestBaseLiquidation):
+
+    # val = """
+    # @payable
+    # @external
+    # # def create_test_position(self, _address: Address, _asset: str, _amount: int) -> None:
+    # #     # Create bad position for testing liquidation. Take out a loan that is too large.
+    # #     # Add ICX collateral via staking contract.
+    # #     pos = self._positions.get_pos(_address)
+    # #     # Mint asset for this position.
+    # #     if _amount > 0:
+    # #         if pos.total_debt() == 0:
+    # #             self._positions.add_nonzero(pos.id.get())
+    # #         self._assets[_asset].mint(_address, _amount)
+    # #         pos[_asset] = pos[_asset] + _amount
+    # #     pos.update_standing()
+    # #     self.check_dead_markets()
+    # # """
+    #
+    # # exec(val)
+    # # setattr(Loans, 'yet_another_method', yet_another_method)
+    # # a = Loans()
+    # # print(a.yet_another_method())
+    #
+    # def test_patch_constants(self):
+    #     sed_cmd = '/def name/a' + "sudeep"
+    #     subprocess.call(['sed', '-i', sed_cmd, "core_contracts/oracle/dummy_oracle.py"])
+    #     # subprocess.call("sed -i -e 's/def name/a _loan/' core_contracts/loans/loans/loans.py", shell=True)
 
     def setUp(self):
         super().setUp()
@@ -13,15 +44,15 @@ class BalancedTestLiquidation(BalancedTestBase):
         for case in test_cases['stories']:
             print("############################################################################################")
             print(case['description'])
-
+            print(self.call_tx(self.contracts['oracle'], 'get_reference_data',
+                                            {'_base': "USD", "_quote": "ICX"}))
             icx = case['actions']['deposited_icx']
-            test_icx = case['actions']['test_icx']
-            test_bnUSD = case['actions']['test_bnUSD']
 
             self.send_tx(self.btest_wallet, self.contracts['loans'], icx, 'depositAndBorrow',
-                         {'_asset': '', '_amount': 0})
+                         {'_asset': 'bnUSD', '_amount': 300*10**18})
             account_position = self.call_tx(self.contracts['loans'], 'getAccountPositions',
                                             {'_owner': self.btest_wallet.get_address()})
+
             self.assertEqual(account_position['standing'], case['actions']['expected_initial_position'],
                              "Error in Account standing of the loan borrower")
             self.assertEqual(account_position['assets']['sICX'], hex(icx),
@@ -29,15 +60,32 @@ class BalancedTestLiquidation(BalancedTestBase):
 
             # This method requires a test method `create_test_position` in the loans contract. If the method is not
             # available, the test case will fail
-            self.send_tx(self.btest_wallet, self.contracts['loans'], test_icx, 'create_test_position',
-                         {'_address': self.btest_wallet.get_address(), '_asset': 'bnUSD', '_amount': test_bnUSD})
+            # self.send_tx(self.btest_wallet, self.contracts['loans'], test_icx, 'create_test_position',
+            #              {'_address': self.btest_wallet.get_address(), '_asset': 'bnUSD', '_amount': test_bnUSD})
+            self.send_tx(self._test1, self.contracts['oracle'], 0, 'set_reference_data',
+                         {'_base': 'USD', '_quote': 'ICX', 'rate': int(5 * 10 ** 18),
+                          'last_update_base': 1902202277702605, 'last_update_quote': 1902202290000000})
             account_position = self.call_tx(self.contracts['loans'], 'getAccountPositions',
                                             {'_owner': self.btest_wallet.get_address()})
             self.assertEqual(account_position['standing'], case['actions']['expected_position'],
                              "Expected position is not liquidate")
 
-            self.send_tx(self.btest_wallet, self.contracts['loans'], method="liquidate",
-                         params={'_owner': self.btest_wallet.get_address()})
-            result = self.call_tx(self.contracts['loans'], "getAccountPositions",
-                                  {'_owner': self.btest_wallet.get_address()})
-            self.assertEqual(result['standing'], case['actions']['expected_result'])
+            # self.send_tx(self._test1, self.contracts['loans'], method="liquidate",
+            #              params={'_owner': self.btest_wallet.get_address()})
+            # # self.setUp()
+            # result = self.call_tx(self.contracts['loans'], "getAccountPositions",
+            #                       {'_owner': self.btest_wallet.get_address()})
+            # self.assertEqual(result['standing'], case['actions']['expected_result'])
+
+    def test_setprice(self):
+        self.call_tx(self.contracts['loans'], 'getAccountPositions',
+                     {'_owner': self.btest_wallet.get_address()})
+        self.send_tx(self.btest_wallet, self.contracts['loans'], method="liquidate",
+                                  params={'_owner': self.btest_wallet.get_address()})
+        # self.send_tx(self.btest_wallet, self.contracts['oracle'], 0, 'set_reference_data',
+        #              {'_base': 'USD', '_quote': 'ICX', 'rate': int(5*10**18), 'last_update_base': 1802202277702605,
+        #               'last_update_quote': 1802202290000000})
+        # print(self.call_tx(self.contracts['oracle'], 'get_reference_data',
+        #                    {'_base': "USD", "_quote": "ICX"}))
+        self.call_tx(self.contracts['loans'], 'getAccountPositions',
+                     {'_owner': self.btest_wallet.get_address()})
