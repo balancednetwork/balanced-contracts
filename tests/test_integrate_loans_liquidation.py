e0e40ad63b2d91df824cc90812580def46cb5c8f
sudeep simkhada <simkhadasudeep9@gmail.com>
2021-06-09 17:37:41 +0545

    rebalancer tests updates

--- tests/test_integrate_loans_liquidation.py
+++ tests/test_integrate_loans_liquidation.py
@@ -7,34 +7,6 @@ from tests.stories.loans.liquidation_stories import LIQUIDATION_STORIES
 
 
 class BalancedTestLiquidation(BalancedTestBaseLiquidation):
-
-    # val = """
-    # @payable
-    # @external
-    # # def create_test_position(self, _address: Address, _asset: str, _amount: int) -> None:
-    # #     # Create bad position for testing liquidation. Take out a loan that is too large.
-    # #     # Add ICX collateral via staking contract.
-    # #     pos = self._positions.get_pos(_address)
-    # #     # Mint asset for this position.
-    # #     if _amount > 0:
-    # #         if pos.total_debt() == 0:
-    # #             self._positions.add_nonzero(pos.id.get())
-    # #         self._assets[_asset].mint(_address, _amount)
-    # #         pos[_asset] = pos[_asset] + _amount
-    # #     pos.update_standing()
-    # #     self.check_dead_markets()
-    # # """
-    #
-    # # exec(val)
-    # # setattr(Loans, 'yet_another_method', yet_another_method)
-    # # a = Loans()
-    # # print(a.yet_another_method())
-    #
-    # def test_patch_constants(self):
-    #     sed_cmd = '/def name/a' + "sudeep"
-    #     subprocess.call(['sed', '-i', sed_cmd, "core_contracts/oracle/dummy_oracle.py"])
-    #     # subprocess.call("sed -i -e 's/def name/a _loan/' core_contracts/loans/loans/loans.py", shell=True)
-
     def setUp(self):
         super().setUp()
 
@@ -65,23 +37,22 @@ class BalancedTestLiquidation(BalancedTestBaseLiquidation):
                                             {'_owner': self.btest_wallet.get_address()})
             self.assertEqual(account_position['standing'], case['actions']['expected_position'],
                              "Expected position is not liquidate")
-
+            time.sleep(25)
             self.send_tx(self._test1, self.contracts['loans'], method="liquidate",
                          params={'_owner': self.btest_wallet.get_address()})
-            time.sleep(20)
             result = self.call_tx(self.contracts['loans'], "getAccountPositions",
                                   {'_owner': self.btest_wallet.get_address()})
             self.assertEqual(result['standing'], case['actions']['expected_result'])
 
-    def test_setprice(self):
-        self.call_tx(self.contracts['loans'], 'getAccountPositions',
-                     {'_owner': self.btest_wallet.get_address()})
-        self.send_tx(self.btest_wallet, self.contracts['loans'], method="liquidate",
-                                  params={'_owner': self.btest_wallet.get_address()})
-        # self.send_tx(self.btest_wallet, self.contracts['oracle'], 0, 'set_reference_data',
-        #              {'_base': 'USD', '_quote': 'ICX', 'rate': int(5*10**18), 'last_update_base': 1802202277702605,
-        #               'last_update_quote': 1802202290000000})
-        # print(self.call_tx(self.contracts['oracle'], 'get_reference_data',
-        #                    {'_base': "USD", "_quote": "ICX"}))
-        self.call_tx(self.contracts['loans'], 'getAccountPositions',
-                     {'_owner': self.btest_wallet.get_address()})
+    # def test_setprice(self):
+    #     self.call_tx(self.contracts['loans'], 'getAccountPositions',
+    #                  {'_owner': self.btest_wallet.get_address()})
+    #     self.send_tx(self.btest_wallet, self.contracts['loans'], method="liquidate",
+    #                               params={'_owner': self.btest_wallet.get_address()})
+    #     # self.send_tx(self.btest_wallet, self.contracts['oracle'], 0, 'set_reference_data',
+    #     #              {'_base': 'USD', '_quote': 'ICX', 'rate': int(5*10**18), 'last_update_base': 1802202277702605,
+    #     #               'last_update_quote': 1802202290000000})
+    #     # print(self.call_tx(self.contracts['oracle'], 'get_reference_data',
+    #     #                    {'_base': "USD", "_quote": "ICX"}))
+    #     self.call_tx(self.contracts['loans'], 'getAccountPositions',
+    #                  {'_owner': self.btest_wallet.get_address()})
