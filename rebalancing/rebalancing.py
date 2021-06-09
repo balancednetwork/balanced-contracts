59ba29d81e3000cd7696a0cdacd0b9b139d82971
Suyog <adhikarisuyog49@gmail.com>
2021-06-09 06:23:16 +0545

    If conditions calculations updated

--- rebalancing/rebalancing.py
+++ rebalancing/rebalancing.py
@@ -129,7 +129,7 @@ class Rebalancing(IconScoreBase):
         redemption_fee = params_loan["redemption fee"]
         sicx_from_lenders = 1*10**18 * price * (POINTS - redemption_fee) // (sicx_rate * POINTS)
         pool_price_dex = self.dex_score.getPriceByName("sICX/bnUSD")
-        if (sicx_from_lenders * pool_price_dex * 10**18) // 10**36> 10**18:
+        if sicx_from_lenders * pool_price_dex > 10**18:
             self.sICX_score.transfer(self._dex.get(), sicx_in_contract, data_bytes)
             bnusd_in_contract = self.bnUSD_score.balanceOf(self.address)
             self.loans_score.returnAsset("bnUSD", bnusd_in_contract)
