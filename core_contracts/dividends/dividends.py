6a593258fbbac0d3f8b87a185b34983e8711209b
galen <galen@mousebelt.com>
2021-06-14 12:39:21 -0700

    [Dividends] Remove on install logic
    There was hotfix logic added to the on install method, this commit removes it
    so the batch deploy method will work properly.

--- core_contracts/dividends/dividends.py
+++ core_contracts/dividends/dividends.py
@@ -199,15 +199,6 @@ class Dividends(IconScoreBase):
         super().on_install()
         self._governance.set(_governance)
 
-        self._accepted_tokens.put(ZERO_SCORE_ADDRESS)
-        loans = self.create_interface_score(Address.from_string('cx66d4d90f5f113eba575bf793570135f9b10cece1'), LoansInterface)
-        day = loans.getDay()
-        self._snapshot_id.set(day)
-        self._max_loop_count.set(MAX_LOOP)
-        self._minimum_eligible_debt.set(MINIMUM_ELIGIBLE_DEBT)
-        self._add_initial_categories()
-        self._distribution_activate.set(False)
-
     def on_update(self) -> None:
         super().on_update()
 
