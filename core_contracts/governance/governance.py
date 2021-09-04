19f917f4378e8700fe7a9da458cc5fa1d8e3ab27
Suyog <adhikarisuyog49@gmail.com>
2021-09-04 23:26:07 +0545

    function name changed from setMaxRetireAmount to setMaxSellAmount

--- core_contracts/governance/governance.py
+++ core_contracts/governance/governance.py
@@ -476,7 +476,7 @@ class Governance(IconScoreBase):
 
     @external
     @only_owner
-    def setMaxRetireAmount(self, _sicx_value: int, _bnusd_value: int) -> None:
+    def setMaxSellAmount(self, _sicx_value: int, _bnusd_value: int) -> None:
         loans = self.create_interface_score(self.addresses['loans'], LoansInterface)
         loans.setMaxSellAmount(_sicx_value, _bnusd_value)
 
