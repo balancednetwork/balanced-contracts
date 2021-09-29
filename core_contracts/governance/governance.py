804de880d916a15c006845453509fc92705dd2c1
simusud <simkhadasudeep9@gmail.com>
2021-09-29 16:12:06 +0545

    added dao disburse to vote action addre daoDisburse internal method added unit tests for the method

--- core_contracts/governance/governance.py
+++ core_contracts/governance/governance.py
@@ -823,11 +823,20 @@ class Governance(IconScoreBase):
         baln = self.create_interface_score(self.addresses['baln'], BalancedInterface)
         baln.setUnstakingPeriod(_time)
 
-    @external
-    @only_owner
-    def daoDisburse(self, _recipient: Address, _amounts: List[Disbursement]) -> None:
+    def daoDisburse(self, _recipient: str, _amounts: List[Disbursement]):
+        if len(_amounts) > 3:
+            revert(f"Cannot disburse more than 3 assets at a time.")
+        _recipient = Address.from_string(_recipient)
+
+        for disbursement in _amounts:
+            disbursement['address'] = Address.from_string(disbursement['address'])
         dao = self.create_interface_score(self.addresses['daofund'], DAOfundInterface)
         dao.disburse(_recipient, _amounts)
+
+    @external
+    @only_owner
+    def daoDistribute(self, _recipient: str, _amounts: List[Disbursement]) -> None:
+        self.daoDisburse(_recipient, _amounts)
 
     @external
     @only_owner
