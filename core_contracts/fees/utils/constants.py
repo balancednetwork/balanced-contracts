03832c19a72f89443a20be0a69aa033705f4cbe7
skyvell <tskyvell@gmail.com>
2021-10-05 23:47:28 +0200

    Added addresses.

--- core_contracts/fees/utils/constants.py
+++ core_contracts/fees/utils/constants.py
@@ -1,2 +1,10 @@
-EXA = 10**18
-POINTS = 10000
+from iconservice import *
+
+# Token addresses. Can be removed after on_install is run.
+IUSDC = "cxae3034235540b924dfcc1b45836c293dcc82bfb7"
+OMM = "cx1a29259a59f463a67bb2ef84398b30ca56b5830a"
+USDS = "cxbb2871f468a3008f80b08fdde5b8b951583acf06"
+CFT = "cx2e6d0fc0eca04965d06038c8406093337f085fcf"
+SICX = "cx2609b924e33ef00b648a409245c7ea394c467824"
+BNUSD = "cx88fd7df7ddff82f7cc735c871dc519838cb235bb"
+BALN = "cxf61cd5a45dc9f91c15aa65831a30a90d59a09619"
