286d62c83af30cfca2579d873b70b1c573b2c9c1
skyvell <tskyvell@gmail.com>
2021-12-20 17:56:46 +0100

    Changed name of class and added descriptive text.

--- core_contracts/rebalancing/utils/enums.py
+++ core_contracts/rebalancing/utils/enums.py
@@ -1,3 +1,7 @@
-class RebalancingType:
+class RebalancingDirection:
+    """
+    Discribes in which direction rebalancing should occur. Forward rebalancing means that sICX will
+    be sold for bnUSD. Reverse rebalancing means bnUSD will be sold for sICX.
+    """
     FORWARD = 0
     REVERSE = 1
